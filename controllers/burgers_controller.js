004d32a097d3fcd16778508c2aca8a9341b0d4cd
John Silkey <jpsilkjr86@gmail.com>
2017-07-29 07:29:26 -0700

    added more handlebars html, got burgers to display from db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,28 @@
 const burger = require('../models/burger.js');
 
 module.exports = (app) => {
+	// route for rendering html to index via handlebars engine
+	app.get('/', (req, res) => {
+		burger.all().then(results => {
+			// get ready burgers and eaten burgers array by sifting through data
+			let eatenBurgers = [];
+			let readyBurgers = [];
+			// loop through results and sort burgers according to devoured value
+			for (let i = 0; i < results.length; i++) {
+				if (results[i].devoured) {
+					eatenBurgers.push(results[i]);
+				}
+				if (!results[i].devoured) {
+					readyBurgers.push(results[i]);
+				}
+			}
+			// render according to arrays
+			res.render('index', {
+				eatenBurgers: eatenBurgers,
+				readyBurgers: readyBurgers
+			});
+		});		
+	});
 	// route for grabbing all burger data
 	app.get('/burgers/all', (req, res) => {
 		burger.all().then(results => {
@@ -10,7 +32,6 @@ module.exports = (app) => {
 			console.log(err);
 		});
 	});
-
 	// route for posting new burger data
 	app.post('/burgers/new', (req, res) => {
 		burger.addNew(req.body.burger_name).then(result => {
@@ -20,7 +41,6 @@ module.exports = (app) => {
 			console.log(err);
 		});
 	});
-
 	// route for eating
 	app.put('/burgers/eat', (req, res) => {
 		burger.eat(req.body.burger_name).then(result => {
@@ -30,7 +50,6 @@ module.exports = (app) => {
 			console.log(err);
 		});
 	});
-
 	// route for ordering more
 	app.put('/burgers/ordermore', (req, res) => {
 		burger.orderMore(req.body.burger_name).then(result => {
