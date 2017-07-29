f18d0dd2f283c54ed50fcfec9bb5eba129d7b603
John Silkey <jpsilkjr86@gmail.com>
2017-07-29 08:39:54 -0700

    added jawsdb connection option

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,18 @@ const keys = require('./keys.js');
 
 // imports mysql npm and establishes connection
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: keys.pw,
-	database: 'burger_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: keys.pw,
+		database: 'burger_db'
+	});
+}
 
 module.exports = connection;
