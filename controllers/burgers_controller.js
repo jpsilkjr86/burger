a5c0df9ba0f2b978afcd992a53683bb172ab8e3c
John Silkey <jpsilkjr86@gmail.com>
2017-07-29 06:17:01 -0700

    successfully set up all routes and got get/post/put requests to communicate properly with database via orm functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,43 @@
 const burger = require('../models/burger.js');
 
 module.exports = (app) => {
-	console.log('so far so good!');
+	// route for grabbing all burger data
+	app.get('/burgers/all', (req, res) => {
+		burger.all().then(results => {
+			res.json(results);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
 
-	burger.all().then(result => {
-		console.log(result);
-	}).catch(err => {
-		console.log(err);
-	}); 
+	// route for posting new burger data
+	app.post('/burgers/new', (req, res) => {
+		burger.addNew(req.body.burger_name).then(result => {
+			res.send(result);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
+
+	// route for eating
+	app.put('/burgers/eat', (req, res) => {
+		burger.eat(req.body.burger_name).then(result => {
+			res.send(result);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
+
+	// route for ordering more
+	app.put('/burgers/ordermore', (req, res) => {
+		burger.orderMore(req.body.burger_name).then(result => {
+			res.send(result);
+		}).catch(err => {
+			res.send(err);
+			console.log(err);
+		});
+	});
 };
